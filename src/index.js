07f6e579cefc0b316782c4b2529e9d182bfc75e4
vladiks-git <ershovvlad18@yandex.ru>
2021-03-12 15:54:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 != 0)
+        return false;
+    let arr = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (str.includes(arr[i])) {
+            str = str.replace(arr[i], "");
+            i = -1;
+        }
+    }
+    if (str.length != 0) {
+        return false;
+    }
+    return true;
 }
